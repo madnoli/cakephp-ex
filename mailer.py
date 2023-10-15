4be2923fd88c393df40e3ab29841357f3098882b
Rajkumar Kantappa Madnoli <58000703+madnoli@users.noreply.github.com>
2023-10-15 20:39:33 +0530

    Update mailer.py

--- mailer.py
+++ mailer.py
@@ -1,15 +1,4 @@
 # Python program to explain shutil.disk_usage() method
 
 # importing shutil module
-import shutil
-
-# Path
-path = "/tmp"
-
-# Get the disk usage statistics
-# about the given path
-stat = shutil.disk_usage(path)
-
-# Print disk usage statistics
-print("Disk usage statistics:")
-print(stat)
+print("Hello from Mailer")
